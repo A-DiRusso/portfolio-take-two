63dcdfdd7dbc7ea379cdc1bacc2cfb6e7a2c637b
Anthony DiRusso <ossurid@gmail.com>
2019-04-19 14:35:44 -0400

    adds 1st layer of style

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+//js
+const exitButton = document.querySelector('[data-close]');
+const nav = document.querySelector('nav');
+
+const navText = document.querySelector('.nav-test');
+const menuBars = document.querySelector('.fa-bars');
+
+function showNav() {
+    nav.classList.remove('hidden');
+    navText.classList.remove('hidden');
+    menuBars.classList.remove('active');
+}
+
+function hideNav() {
+    nav.classList.add('hidden');
+    navText.classList('hidden');
+    menuBars.classList.add('active');
+}
+
+exitButton.addEventListener('click', hideNav);
+nav.addEventListener('mouseover', showNav);
+nav.addEventListener('mouseleave', hideNav);
